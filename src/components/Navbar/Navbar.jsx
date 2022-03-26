8262ea995ef637a4c29d844cfc3678259cc3dd4b
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-26 17:38:58 +0530

    feat: add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+
+import './Navbar.css';
+
+export default function Navbar() {
+    return <nav>
+    <div className="left-nav">
+        <a href="/index.html">CartMax</a>
+ </div>
+ <ul className="navbar-search">
+    <input className="search-box" type="search" placeholder="Search" /><span className="search-icon">
+   <i className="fas fa-search" aria-hidden="true"></i></span>
+  </ul>
+    <ul className="right-nav">
+        <li><a href="../../login.jsx" className="btn btn-default">Login</a></li>
+        <li className="badge"><a href="/pages/cart.html">
+            <i className="badge-icon fas fa-cart-plus"></i>
+            <span className="badge-number">0</span> 
+        </a></li>
+        <li className="badge"><a href="/pages/wishlist.html">
+            <i className="badge-icon fas fa-heart" aria-hidden="true"></i>
+            <span className="badge-number">0</span> 
+        </a></li>
+  
+    </ul>
+</nav>
+}
