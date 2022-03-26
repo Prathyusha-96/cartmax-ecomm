649416b8a42a0a1348215587da765c024d34c4d4
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-26 17:38:35 +0530

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+export default function Footer() {
+    return <footer> 
+        <div className="footer">
+    <p className="footer-text">Made with <i className="fas fa-heart"></i> by Prathyusha</p>
+    <div className="footer-icon">
+      <a href="#"><i className="icon fab fa-github-square"></i></a>
+      <a href="#"><i className="icon fab fa-instagram-square"></i></a>
+      <a href="#"><i className="icon fab fa-linkedin"></i></a>
+    </div>   
+    </div> 
+  </footer>
+}
