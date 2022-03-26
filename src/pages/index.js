229ade5f985693c51de5c1b8dfc962ae9b6e37ff
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-26 17:45:58 +0530

    refactor: exports  files

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+
+
+import Landingpage from "./Landing-page/Landingpage";
+import Login from "./Authentication/Login";
+import Signup from "./Authentication/Signup";
+import Cart  from "./Cart/Cart";
+import Products from "./Products/Products";
+import Filter from "./Products/components/Filter";
+
+ export { Landingpage, Login, Signup, Cart, Products, Filter }
