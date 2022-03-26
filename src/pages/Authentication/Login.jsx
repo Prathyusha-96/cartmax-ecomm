115d1cb1c6e637663099090032fcd8498b404b48
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-26 17:39:24 +0530

    feat: add authentication

---
+++ src/pages/Authentication/Login.jsx
@@ -0,0 +1,36 @@
+
+ 
+ import './auth.css'
+export default function Login() {
+    return (
+        
+    <main className="container">
+        
+        <div className="login-form">
+            <div className="login-title">
+                <h2 className="heading text-center">Login</h2>
+            </div>
+            <div className="input">
+                <label>Email</label>
+                <input className="input-txt" type="email" />
+            </div>
+            <div className="input">
+                <label>Password</label>
+                <input className="input-txt" type="password" />
+            </div>
+            <div className="input">
+                <label className="input-checkbox"></label>
+
+                <a href="#" className="login-forget">Forget your Password?</a>
+            </div>
+            
+            <div className="btn-signup text-center">
+                <button className="btn btn-primary">Login</button>
+            </div>
+            <div className="text-center">
+               <span>Create New Account <a className="login-link fw-400" href="/pages/signup.html">Signup</a></span>
+            </div>
+        </div>
+    </main>
+     )
+}
---
+++ src/pages/Authentication/Signup.jsx
@@ -0,0 +1,33 @@
+import '../Landing-page/landingpage.css'
+import './auth.css'
+export default function Signup() {
+    return(
+        
+        <main className="container">
+        <div className="login-form">
+            <div className="login-title">
+                <h2 className="heading text-center">Signup</h2>
+            </div>
+            <div className="input">
+                <label>Full Name</label>
+                <input className="input-txt" type="name" />
+            </div>
+            <div className="input">
+                <label>Email</label>
+                <input className="input-txt" type="email" />
+            </div>
+            <div className="input">
+                <label>Password</label>
+                <input className="input-txt" type="password" />
+            </div>
+            <div className="btn-signup text-center">
+                <button className="btn btn-primary">Create New Account</button>
+            </div>
+            <div className="text-center">
+            <span>Allready have an Account  <a className="login-link fw-400" href="/pages/login.html">Login</a></span>
+            </div>
+        </div>
+    </main> 
+    
+    )
+}
