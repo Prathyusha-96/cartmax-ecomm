81282957bd8dace080415391252fb4bb1d26d7cd
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-29 15:00:35 +0530

    import files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import "./App.css";
-import  Landingpage from "./pages/Landing-page/Landingpage";
+
+
 import Cart  from "./pages/Cart/Cart";
-
-
+import Products from "./pages/Products/Products"
+import  Login  from "./pages/Authentication/Login";
+import Signup  from "./pages/Authentication/Signup";
+import Wishlist  from "./pages/Wishlist/Wishlist";
+import  Landingpage from "./pages/Landing-page/Landingpage";
 
 function App() {
   return (
     <div className="App">
-  <Landingpage />
-  <Cart />
- </div>
-  );
+  < Landingpage />
+    < Products />
+    < Cart />
+    < Login />
+    < Signup />
+    < Wishlist/> 
+    </div> 
+  )
 }
 
 export default App;
