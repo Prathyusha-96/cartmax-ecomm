ece054a41a916440d669b19da6d9ed1173d2e4d3
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-29 15:01:24 +0530

    added some text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 import './Footer.css'
 export default function Footer() {
-    return <footer> 
+    return (
+     
+    <footer> 
         <div className="footer">
-    <p className="footer-text">Made with <i className="fas fa-heart"></i> by Prathyusha</p>
+    <p className="footer-text">Made with<i className="icon fa fa-heart"></i>by Prathyusha </p>
     <div className="footer-icon">
       <a href="#"><i className="icon fab fa-github-square"></i></a>
       <a href="#"><i className="icon fab fa-instagram-square"></i></a>
       <a href="#"><i className="icon fab fa-linkedin"></i></a>
-    </div>   
+    </div>  
+    <p className="footer-text">copyright 2022 CartMax All rights reserved</p>
     </div> 
   </footer>
+ 
+    )
 }
