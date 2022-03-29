faa39dcfeaa92026252a0157590efc899e18656d
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-03-29 15:07:48 +0530

    refactor: add file paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Login from "./Authentication/Login";
 import Signup from "./Authentication/Signup";
 import Cart  from "./Cart/Cart";
 import Products from "./Products/Products";
-import Filter from "./Products/components/Filter";
+import Filter from "./Products/Filter";
+import Wishlist from "./Wishlist/Wishlist";
 
- export { Landingpage, Login, Signup, Cart, Products, Filter }
+ export {Landingpage, Login, Signup, Cart, Products, Filter, Wishlist }
