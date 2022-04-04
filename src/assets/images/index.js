1317ef8f707e879ee9da0c02f57e408ef833fe68
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-04-04 17:33:33 +0530

    refactor: import all images

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import longkurti       from '../../assets/images/product1.jpg'
+import handbag         from '../../assets/images/product2.jpg';
+import shoes           from '../../assets/images/product3.jpg';
+import premiumjacket   from '../../assets/images/product4.jpg';
+import kidsdress       from '../../assets/images/product5.jpg';
+import denimjacket     from '../../assets/images/product6.jpg';
+import kidsjeans       from '../../assets/images/product10.jpg';
+import womenkurti      from '../../assets/images/product11.jpg';
+import printedkurti    from '../../assets/images/product12.jpg';
+import bluekurti       from '../../assets/images/product13.jpg';
+import levisbag        from '../../assets/images/product14.jpg';
+import levisbag2       from '../../assets/images/product15.jpg';
+import wallet          from '../../assets/images/product16.jpg';
+import girlsfrock      from '../../assets/images/product17.jpg';
+import longfrock       from '../../assets/images/product18.jpg';
+import shirt           from '../../assets/images/product19.jpg';
+import tshirt          from '../../assets/images/product20.jpg';
+import leatherjacket   from '../../assets/images/product21.jpg';
+
+export{ longkurti,handbag,shoes,premiumjacket,kidsdress, denimjacket,kidsjeans, womenkurti, printedkurti, bluekurti,levisbag, levisbag2,
+    wallet,girlsfrock, longfrock,  shirt, tshirt, leatherjacket}
