55082873c24d95ce73ec81069e11b20e91663674
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-04-05 10:47:55 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import { Footer, Navbar } from "./components";
-import { Login, Signup, Landingpage, Cart, Wishlist, Products } from "./pages";
-
+import { Login, Signup, Landingpage, Cart, Wishlist } from "./pages";
+import MockmanEs from "mockman-js";
+import { ProductListing } from './pages/Products/ProductListing';
 function App() {
   return (
     <div className="App">
       <Navbar />
      <Routes>
-     <Route path="/" element={< Landingpage />} />
-     <Route path="/Products" element={<Products />} />
+    
+     <Route path="/" element={<Landingpage />} />
      <Route path="/Login" element={<Login />} />
      <Route path="/Signup" element={<Signup />} />
      <Route path="/Cart" element={<Cart />} />
      <Route path="/Wishlist" element={<Wishlist/>} />
+     <Route path="/Products" element={<ProductListing/>} />
+
+     <Route path="/mock" element={<MockmanEs />} />
 </Routes>
  <Footer/>
     </div> 
