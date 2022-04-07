922cda50016acb5175bbd2e6798026671d06ea33
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-04-07 12:13:59 +0530

    import productListing file

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,6 @@ import Login from "./Authentication/Login";
 import Signup from "./Authentication/Signup";
 import Cart  from "./Cart/Cart";
 import Wishlist from "./Wishlist/Wishlist";
+import { ProductListing } from "./Products/ProductListing";
 
- export {Landingpage, Login, Signup, Cart,  Wishlist }
+ export {Landingpage, Login, Signup, Cart,  Wishlist, ProductListing }
