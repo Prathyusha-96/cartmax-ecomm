79dc8522e7f29856a91e9c721977d8e6587a666a
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-04-07 11:45:59 +0530

    refactor: add filters in utils

---
+++ src/utils/categoryFilter.js
@@ -0,0 +1,34 @@
+const categoryFilter = (data, { mensClothing, womensClothing, womenAccessories, kidsClothing }) => {
+
+    if (!mensClothing && !womensClothing && !womenAccessories && !kidsClothing) {
+        return data;
+    }
+
+    const filteredData = [];
+
+    if (mensClothing) {
+       
+        let newData = data.filter((item) => item.categoryName === "mens-clothing");
+        filteredData.push(...newData);
+    }
+    if (womensClothing) {
+        
+        let newData = data.filter((item) => item.categoryName === "womens-clothing");
+        filteredData.push(...newData);
+    }
+    if (womenAccessories) {
+        
+        let newData = data.filter((item) => item.categoryName === "women-accessories");
+        filteredData.push(...newData);
+    }
+    
+    if (kidsClothing) {
+      
+        let newData = data.filter((item) => item.categoryName === "kids-clothing");
+        filteredData.push(...newData);
+    }
+
+    return filteredData;
+};
+
+export { categoryFilter };
