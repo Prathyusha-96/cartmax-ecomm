3f9bbcef28365812b8b98966d244af745d2fe08d
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-04-07 11:39:55 +0530

    import filter provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { makeServer } from "./server";
+import {FilterProvider} from "./hooks";
 
 
 // Call make Server
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <FilterProvider>
     <App/>
+    </FilterProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
