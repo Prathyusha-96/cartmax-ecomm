eee47e2c158fdb3b172f74583049c93b219f2b25
Prathyusharam <89379190+Prathyusharam@users.noreply.github.com>
2022-04-11 10:52:55 +0530

    refactor:  import authprovider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 
 
 import Landingpage from "./Landing-page/Landingpage";
-import Login from "./Authentication/Login";
-import Signup from "./Authentication/Signup";
+import { Login } from "./Authentication/Login";
+import { SignUp } from "./Authentication/SignUp";
 import Cart  from "./Cart/Cart";
 import Wishlist from "./Wishlist/Wishlist";
 import { ProductListing } from "./Products/ProductListing";
 
- export {Landingpage, Login, Signup, Cart,  Wishlist, ProductListing }
+ export {Landingpage, Login, SignUp, Cart,  Wishlist, ProductListing }
