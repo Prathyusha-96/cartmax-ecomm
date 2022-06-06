b936ddbad5738d0490b6736ca3d2109300e18144
Prathyusharam <prathyushaa1996@gmail.com>
2022-06-06 17:57:56 +0530

    feat: add react-tostify

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React from "react";
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import { Footer, Navbar } from "./components";
-import { Login, Signup, Landingpage, Cart, Wishlist } from "./pages";
+import { Login, Signup, Landingpage, Cart, Wishlist,   ProductListing } from "./pages/index";
 import MockmanEs from "mockman-js";
-import { ProductListing } from './pages/Products/ProductListing';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="colored"
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Navbar />
+     
      <Routes>
-    
      <Route path="/" element={<Landingpage />} />
      <Route path="/Login" element={<Login />} />
      <Route path="/Signup" element={<Signup />} />
      <Route path="/Cart" element={<Cart />} />
      <Route path="/Wishlist" element={<Wishlist/>} />
      <Route path="/Products" element={<ProductListing/>} />
-
      <Route path="/mock" element={<MockmanEs />} />
 </Routes>
  <Footer/>
